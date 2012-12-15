3ce4014dfed0f68129450ff018dee773fbb9cc79
alinamalik <alina.malik5@gmail.com>
2012-12-15 20:45:19 +0500

    Done with chapter no 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LearningRails::Application.routes.draw do
+
+  match '/about' =>  'pages#about'
+  match '/contact' => 'pages#contact'
+
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
